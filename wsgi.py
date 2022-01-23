adfe4ca32aaa8030e769f66ba55b09fb99edcc7e
Oshinko <osnk@renjaku.jp>
2022-01-23 18:24:03 +0900

    Update webhooks

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,12 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/echo/<message>')
+def echo(message):
+    return message
+
+
 @app.route('/shutdown', methods=['POST'])
 def shutdown():
-    subprocess.run('shutdown /s /t 120', shell=True)
+    subprocess.run('shutdown /s /t 10', shell=True)
     return '', 204
